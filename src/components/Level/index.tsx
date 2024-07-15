228681012bfaaa79f4067b5289596a8b527034b6
Lamenkazu <333.erick@gmail.com>
2024-07-15 09:43:02 -0300

    ✨ Pilares do REanimated: Estilização reagindo ao useSharedValue

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,4 +1,5 @@
-import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import { Pressable, PressableProps, Text, View } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 import { THEME } from '../../styles/theme';
 import { styles } from './styles';
@@ -9,21 +10,41 @@ const TYPE_COLORS = {
   MEDIUM: THEME.COLORS.WARNING_LIGHT,
 }
 
-type Props = TouchableOpacityProps & {
+type Props = PressableProps & {
   title: string;
   isChecked?: boolean;
   type?: keyof typeof TYPE_COLORS;
 }
 
 export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
-
   const COLOR = TYPE_COLORS[type];
+  
+  const scale = useSharedValue(1)
+  const animatedContainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{scale: scale.value}]
+    }
+  })
+
+  const onPressIn = () => {
+    scale.value = 1.1
+  }
+
+  const onPressOut = () => {
+    scale.value = 1
+
+  }
+
 
   return (
-    <TouchableOpacity {...rest}>
-      <View style={
+    <Pressable
+    onPressIn={onPressIn}
+    onPressOut={onPressOut}
+    {...rest}>
+      <Animated.View style={
         [
           styles.container,
+          animatedContainerStyle,
           { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
         ]
       }>
@@ -34,7 +55,7 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
           ]}>
           {title}
         </Text>
-      </View>
-    </TouchableOpacity>
+      </Animated.View>
+    </Pressable>
   );
 }
